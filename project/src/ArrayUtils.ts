78a0aa1da0aea0090cdcb5833092ee5c79995f39
Nils Gawlik <nilsgawlik@gmx.de>
2017-08-04 12:05:32 +0200

    Add a class for array utility functions

---
+++ project/src/ArrayUtils.ts
@@ -0,0 +1,14 @@
+class ArrayUtils {
+    static get2DArray<T>(width : number, height : number, initialValue : T) : T[][] {
+        let array : any = [];
+
+        for(let y = 0; y < height; y++){
+            array.push([]);
+            for(let x = 0; x < width; x++) {
+                array[x].push(initialValue);
+            }
+        }
+
+        return array as T[][];
+    }
+}
