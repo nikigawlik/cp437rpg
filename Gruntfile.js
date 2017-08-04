44e39a1443c68a2e0dea0030be426f4ba01188d8
Nils Gawlik <nilsgawlik@gmx.de>
2017-08-04 11:31:44 +0200

    Enable strict null chacks for TypeScript

--- Gruntfile.js
+++ Gruntfile.js
@@ -24,6 +24,7 @@ module.exports = function (grunt) {
                 outDir: 'project/test',
                 options: {
                     rootDir: 'project/test',
+                    strictNullChecks: true,
                 }
             }
         },
