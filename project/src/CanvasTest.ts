3353ab1755f9b7dcaf6c401c41725fd6f6951e9d
Nils Gawlik <nilsgawlik@gmx.de>
2017-08-25 13:58:57 +0200

    change canvas testing to random tiles

--- project/src/CanvasTest.ts
+++ project/src/CanvasTest.ts
@@ -2,14 +2,11 @@ class CanvasTest {
     constructor() {
         let canvas = game.canvas;
 
-        canvas.setTile(0, 0, 0);
-        canvas.setTile(2, 0, 1);
-        canvas.setTile(1, 0, 2);
-        canvas.setTile(3, 0, 3);
-        canvas.setTile(4, 0, 4);
-        canvas.setTile(6, 0, 5);
-        canvas.setTile(7, 0, 6);
-        canvas.setTile(8, 0, 7);
-        canvas.setTile(9, 0, 8);
+        for(let x = 0; x < canvas.width; x++)
+        for(let y = 0; y < canvas.height; y++)
+        {
+            let tileID = Math.floor(Math.random() * 256);
+            canvas.setTile(x, y, tileID);
+        }
     }
 }
