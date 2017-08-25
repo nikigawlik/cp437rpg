c96fcc4b9dc1c703cd3df9dddb0b3b6346956939
Nils Gawlik <nilsgawlik@gmx.de>
2017-08-25 12:54:24 +0200

    Change var to let

--- project/src/Tileset.ts
+++ project/src/Tileset.ts
@@ -18,14 +18,14 @@ class Tileset {
     getTileArea(tileId : number) : Rect | null {
         if (tileId < 0 || tileId >= this.tilesHorizontal * this.tilesVertical) { return null; }
 
-        var xx : number = tileId % this.tilesHorizontal;
-        var yy : number = Math.floor(tileId / this.tilesHorizontal);
+        let xx : number = tileId % this.tilesHorizontal;
+        let yy : number = Math.floor(tileId / this.tilesHorizontal);
         return new Rect(xx * this.tileWidth, yy * this.tileHeight, this.tileWidth, this.tileHeight);
     }
 
     drawTile(canvas : HTMLCanvasElement, tileId : number, x : number, y : number, w : number, h : number) {
-        var tile : Rect | null = this.getTileArea(tileId);
-        var ctx : CanvasRenderingContext2D | null = canvas.getContext("2d");
+        let tile : Rect | null = this.getTileArea(tileId);
+        let ctx : CanvasRenderingContext2D | null = canvas.getContext("2d");
         if (ctx === null || tile === null) { return; }
 
         ctx.drawImage(this.image, tile.x, tile.y, tile.w, tile.h, x, y, w, h);
