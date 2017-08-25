7115e644de6f29278f0525bd9edb0f8fc2a97eb4
Nils Gawlik <nilsgawlik@gmx.de>
2017-08-25 13:04:59 +0200

    Support global background color

--- project/src/Canvas.ts
+++ project/src/Canvas.ts
@@ -3,6 +3,7 @@ class Canvas {
     height : number;
     displayGrid : number[][];
     canvas : HTMLCanvasElement;
+    backgroundStyle : String = "black";
     
     constructor(width : number, height : number) {
         this.width = Math.floor(width);
@@ -14,6 +15,15 @@ class Canvas {
         this.canvas = document.createElement("canvas");
         this.canvas.width = this.width * game.tileset.tileWidth;
         this.canvas.height = this.height * game.tileset.tileHeight;
+
+
+        let ctx : CanvasRenderingContext2D | null = this.canvas.getContext("2d");
+        if (!ctx) {return;}
+        // set fill style
+        ctx.fillStyle = "black";
+
+        // clear canvas 
+        ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
 
         // append canvas to page
         let container : HTMLElement | null = document.getElementById("canvas_container");
@@ -28,6 +38,18 @@ class Canvas {
     }
 
     refreshTile(x, y) {
+        let ctx : CanvasRenderingContext2D | null = this.canvas.getContext("2d");
+        if (!ctx) {return;}
+
+        // draw background
+        ctx.fillRect(
+            x * game.tileset.tileWidth, 
+            y * game.tileset.tileHeight, 
+            game.tileset.tileWidth, 
+            game.tileset.tileHeight
+        )
+
+        // draw foreground
         game.tileset.drawTile(
             this.canvas, 
             this.displayGrid[x][y], 
