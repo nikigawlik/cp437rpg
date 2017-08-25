0a5083dfc177b54b3ab5017996f70bf3c2b16959
Nils Gawlik <nilsgawlik@gmx.de>
2017-08-25 12:36:13 +0200

    Add Rect class to model rectangles

---
+++ project/src/Rect.ts
@@ -0,0 +1,12 @@
+class Rect {
+    public x : number;
+    public y : number;
+    public w : number;
+    public h : number;
+    constructor(x : number, y : number, w : number, h : number) {
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+    }
+}
