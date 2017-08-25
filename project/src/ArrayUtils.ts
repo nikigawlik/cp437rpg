d5c1394c73263381de4538161e10a943c3199b7b
Nils Gawlik <nilsgawlik@gmx.de>
2017-08-25 13:58:32 +0200

    Fix array generator x and y order

--- project/src/ArrayUtils.ts
+++ project/src/ArrayUtils.ts
@@ -2,10 +2,10 @@ class ArrayUtils {
     static get2DArray<T>(width : number, height : number, initialValue : T) : T[][] {
         let array : any = [];
 
-        for(let y = 0; y < height; y++){
+        for(let x = 0; x < width; x++) {
             array.push([]);
-            for(let x = 0; x < width; x++) {
-                array[y].push(initialValue);
+            for(let y = 0; y < height; y++){
+                array[x].push(initialValue);
             }
         }
 
