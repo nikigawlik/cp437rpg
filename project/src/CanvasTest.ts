03685d3d3a3645751b488ac933d3b648d528df5a
Nils Gawlik <nilsgawlik@gmx.de>
2017-08-25 12:38:45 +0200

    Set up test canvas

---
+++ project/src/CanvasTest.ts
@@ -0,0 +1,15 @@
+class CanvasTest {
+    constructor() {
+        let canvas = game.canvas;
+
+        canvas.setTile(0, 0, 0);
+        canvas.setTile(2, 0, 1);
+        canvas.setTile(1, 0, 2);
+        canvas.setTile(3, 0, 3);
+        canvas.setTile(4, 0, 4);
+        canvas.setTile(6, 0, 5);
+        canvas.setTile(7, 0, 6);
+        canvas.setTile(8, 0, 7);
+        canvas.setTile(9, 0, 8);
+    }
+}
---
+++ project/src/main.ts
@@ -0,0 +1,12 @@
+/**
+ * This file initializes the whole engine and should be executed last
+ */
+let game : Game;
+window.onload = function () {
+    game = new Game(SettingsManager.loadGameSettings());
+    game.intialize();
+
+    setTimeout(function () {
+        let ct = new CanvasTest();
+    }, 300);
+}
