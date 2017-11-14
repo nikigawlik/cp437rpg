0dc8bc36d23d00ef3f866fd96f86d30a4ff70439
Nils Gawlik <nilsgawlik@gmx.de>
2017-11-14 12:31:35 +0100

    Fix walking out of world error

--- project/src/GameObject.ts
+++ project/src/GameObject.ts
@@ -13,6 +13,13 @@ class GameObject {
     }
     
     public moveRelative(dx : number, dy : number) {
+        // check some collisions
+        if(this.x + dx > game.world.width-1 || this.x + dx < 0
+        || this.y + dy > game.world.height-1 || this.y + dy < 0) {
+            return;    
+        }
+
+        // all ok
         game.world.removeObject(this);
         this.x += dx;
         this.y += dy;
