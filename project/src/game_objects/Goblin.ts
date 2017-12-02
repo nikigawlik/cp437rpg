7999bfa02ffdce5a9256d79648fe0fa5d90be576
Nils Gawlik <nilsgawlik@gmx.de>
2017-12-02 20:34:11 +0100

    Change Goblin timers to be more predictable

--- project/src/game_objects/Goblin.ts
+++ project/src/game_objects/Goblin.ts
@@ -1,3 +1,8 @@
+enum GoblinStates {
+    Follow,
+    Idle,
+}
+
 class Goblin extends GameObject implements Continuous {
     private aiCounter = 0;
     private aiDelay = 15;
@@ -7,32 +12,47 @@ class Goblin extends GameObject implements Continuous {
     private moveCounter : number = 0;
     private moveDelay : number = 6;
 
+    private cantSeeCounter : number = 0;
+    private cantSeeDelay : number = 120;  // steps it takes to stop chase
+
+    private state : number = GoblinStates.Idle;
+
     constructor(x : number, y : number) {
         super(x, y, new Tile(71, 2, 0))
         game.addContinuous(this);
 
         this.aiCounter = this.aiDelay;
+        this.cantSeeCounter = this.cantSeeDelay;
     }
 
     public update() {
-        this.aiCounter = Math.max(this.aiCounter - 1, 0);
-        if (this.aiCounter === 0) {
-            this.aiCounter = this.aiDelay
-            // lose old target
-            this.target = null;
-            // scan for player
-            let done : boolean = false;
-            for (let dx : number = -this.visionRange; dx <= this.visionRange && !done; dx++) 
-            for (let dy : number = -this.visionRange; dy <= this.visionRange && !done; dy++) {
-                let obj : GameObject | null = game.world.getTopAt(this.x + dx, this.y + dy);
-                if (obj !== null && obj instanceof Player) {
-                    // calc line of sight
-                    if (game.world.canSee(this.x, this.y, obj.x, obj.y)) {
-                        this.target = obj;
-                        done = true;
+        switch (this.state) {
+            case GoblinStates.Idle: 
+                // exit condition
+                if (this.target !== null) {
+                    this.state = GoblinStates.Follow;
+                    this.aiCounter = this.aiDelay;
+                    break;
+                }
+                this.aiCounter = Math.max(this.aiCounter - 1, 0);
+                if (this.aiCounter === 0) {
+                    this.scanForPlayerTarget();
+                }
+            break;
+            case GoblinStates.Follow: 
+                // exit condition
+                if (this.target === null) {
+                    this.state = GoblinStates.Idle;
+                    break;
+                }
+                if (!game.world.canSee(this.x, this.y, this.target.x, this.target.y)) {
+                    this.cantSeeCounter--;
+                    if (this.cantSeeCounter <= 0) {
+                        this.cantSeeCounter = this.cantSeeDelay;
+                        this.target = null;
                     }
                 }
-            }
+            break;
         }
 
         // movement
@@ -59,6 +79,23 @@ class Goblin extends GameObject implements Continuous {
         if (this.moveCounter === 0 && (dx != 0 || dy != 0)) {
             this.moveCounter = this.moveDelay;
             this.moveRelative(dx, dy);
+        }
+    }
+
+    private scanForPlayerTarget() {
+        this.aiCounter = this.aiDelay
+        // scan for player
+        let done : boolean = false;
+        for (let dx : number = -this.visionRange; dx <= this.visionRange && !done; dx++) 
+        for (let dy : number = -this.visionRange; dy <= this.visionRange && !done; dy++) {
+            let obj : GameObject | null = game.world.getTopAt(this.x + dx, this.y + dy);
+            if (obj !== null && obj instanceof Player) {
+                // calc line of sight
+                if (game.world.canSee(this.x, this.y, obj.x, obj.y)) {
+                    this.target = obj;
+                    done = true;
+                }
+            }
         }
     }
 }
